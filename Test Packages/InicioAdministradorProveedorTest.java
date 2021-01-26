72ae979fe7f529f222d27fc3301ff188e00f6ec4
Lorenacm2610 <lorena.manuel2610@gmail.com>
2021-01-26 14:17:54 +0100

    Test acabado. Corregir excepciones + datapicker restriccion

--- Test Packages/InicioAdministradorProveedorTest.java
+++ Test Packages/InicioAdministradorProveedorTest.java
@@ -1,21 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 import applicationClient.ApplicationClient;
+import javafx.application.Application;
 import javafx.scene.Node;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
+import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 import static org.testfx.api.FxAssert.verifyThat;
+import org.testfx.api.FxToolkit;
 import org.testfx.framework.junit.ApplicationTest;
 import org.testfx.matcher.base.NodeMatchers;
 import static org.testfx.matcher.base.NodeMatchers.isDisabled;
 import static org.testfx.matcher.base.NodeMatchers.isEnabled;
-import static org.testfx.matcher.base.NodeMatchers.isFocused;
 import static org.testfx.matcher.base.NodeMatchers.isVisible;
 import static org.testfx.util.NodeQueryUtils.hasText;
 
@@ -32,30 +30,37 @@ public class InicioAdministradorProveedorTest extends ApplicationTest {
      * @param stage, Objeto Stage
      * @throws Exception, si ocurre algun error
      */
-    public void start(Stage stage) throws Exception {
-        new ApplicationClient().start(stage);
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        FxToolkit.registerPrimaryStage();
+        FxToolkit.setupApplication(ApplicationClient.class);
 
     }
 
-    /*
+    /**
+     * Comienza la ventana LogIn, introducimos las credenciales del
+     * administrador para poder acceder a la ventana
+     * InicioAdministradorProveedor
+     */
     @Test
-    public void testA_InitialState() {
+    public void preTest() {
+        sleep(3000);
         clickOn("#txtUsuario");
-        write("administrador");
+        write("admin");
         clickOn("#txtContrasena");
-        write("administrador");
+        write("admin");
         clickOn("#btnIniciar");
         verifyThat("#pnInicioAdminVend", isVisible());
         clickOn("#menuProveedor");
         clickOn("#menuProveedores");
         verifyThat("#pnInicioAdminProv", isVisible());
-    }*/
+    }
 
     /**
      * Test que permite ver el estado inicial de la ventana
      */
     @Test
-    public void testB_showStage() {
+    public void testA_showStage() {
         verifyThat("#tfBuscar", hasText(""));
         verifyThat("#btnBorrarProveedor", isDisabled());
         verifyThat("#btnAltaProveedor", isEnabled());
@@ -63,33 +68,197 @@ public class InicioAdministradorProveedorTest extends ApplicationTest {
     }
 
     @Test
-    public void testC_menuSalir() {
+    public void testB_comprobacionDataPicker() {
+        Node rowFecha = lookup("#tcFechaAlta").nth(1).query();
+        doubleClickOn(rowFecha);
+        clickOn(1315, 275);
+        clickOn("16");
+    }
+
+    @Test
+    public void testC_NuevoProveedorConValidaciones() {
+        clickOn("#btnAltaProveedor");
+        Node rowOrdenNombre = lookup("#tcNombre").nth(0).query();
+        clickOn(rowOrdenNombre);
+        //Nombre
+        Node rowNombre = lookup("#tcNombre").nth(1).query();
+        doubleClickOn(rowNombre);
+        write("Antón García");
+        this.push(KeyCode.ENTER);
+        ///Tipo producto
+        //Node rowOrdenTipoProducto = lookup("#tcTipo").nth(0).query();
+        //clickOn(rowOrdenTipoProducto);
+        Node rowTipo = lookup("#tcTipo").nth(1).query();
+        rowTipo.isFocused();
+        doubleClickOn(rowTipo);
+        clickOn(rowTipo);
+        //Orden Empresa
+        Node rowOrdenEmpresa = lookup("#tcEmpresa").nth(0).query();
+        clickOn(rowOrdenEmpresa);
+        //Empresa
+        Node rowEmpresa = lookup("#tcEmpresa").nth(1).query();
+        doubleClickOn(rowEmpresa);
+        write("Roxy");
+        this.push(KeyCode.ENTER);
+        //Orden Email
+        Node rowOrdenEmail = lookup("#tcEmail").nth(0).query();
+        clickOn(rowOrdenEmail);
+        //Email
+        Node rowEmail = lookup("#tcEmail").nth(1).query();
+        doubleClickOn(rowEmail);
+        write("roxy@gmail.com");
+        this.push(KeyCode.ENTER);
+        //Orden Teléfono
+        Node rowOrdenTelefono = lookup("#tcTelefono").nth(0).query();
+        clickOn(rowOrdenTelefono);
+        //Teléfono
+        Node rowTelefono = lookup("#tcTelefono").nth(1).query();
+        doubleClickOn(rowTelefono);
+        write("615478520");
+        this.push(KeyCode.ENTER);
+        //Orden Descripción
+        Node rowOrdenDescripcion = lookup("#tcDescripcion").nth(0).query();
+        clickOn(rowOrdenDescripcion);
+        //Descripción
+        Node rowDescripcion = lookup("#tcDescripcion").nth(1).query();
+        doubleClickOn(rowDescripcion);
+        write("Producto de buena calidad y rápida entrega");
+        this.push(KeyCode.ENTER);
+
+    }
+
+    @Test
+    public void testD_botonBorrarAceptar() {
+        Node row = lookup("#tcNombre").nth(1).query();
+        clickOn(row);
+        verifyThat("#btnBorrarProveedor", isEnabled());
+        clickOn("#btnBorrarProveedor");
+        verifyThat("Aceptar", NodeMatchers.isVisible());
+        verifyThat("Cancelar", NodeMatchers.isVisible());
+        clickOn("Aceptar");
+    }
+
+    @Test
+    public void testE_botonBorrarCancelar() {
+        Node row = lookup("#tcNombre").nth(1).query();
+        clickOn(row);
+        clickOn("#btnBorrarProveedor");
+        verifyThat("#btnBorrarProveedor", isEnabled());
+        verifyThat("Aceptar", NodeMatchers.isVisible());
+        verifyThat("Cancelar", NodeMatchers.isVisible());
+        clickOn("Cancelar");
+    }
+
+    @Test
+    public void testF_AlertaNombre() {
+        clickOn("#btnAltaProveedor");
+        Node rowOrdenarNombre = lookup("#tcNombre").nth(0).query();
+        clickOn(rowOrdenarNombre);
+        Node rowNombre = lookup("#tcNombre").nth(1).query();
+        doubleClickOn(rowNombre);
+        write(".");
+        this.push(KeyCode.ENTER);
+        verifyThat("Aceptar", NodeMatchers.isVisible());
+        clickOn("Aceptar");
+    }
+
+    @Test
+    public void testG_AlertaEmpresa() {
+        Node rowOrdenarEmpresa = lookup("#tcEmpresa").nth(0).query();
+        clickOn(rowOrdenarEmpresa);
+        Node rowEmpresa = lookup("#tcEmpresa").nth(1).query();
+        doubleClickOn(rowEmpresa);
+        write(".");
+        this.push(KeyCode.ENTER);
+        verifyThat("Aceptar", NodeMatchers.isVisible());
+        clickOn("Aceptar");
+    }
+
+    @Test
+    public void testH_AlertaEmail() {
+        Node rowOrdenarEmail = lookup("#tcEmail").nth(0).query();
+        clickOn(rowOrdenarEmail);
+        Node rowEmail = lookup("#tcEmail").nth(1).query();
+        doubleClickOn(rowEmail);
+        write(".");
+        this.push(KeyCode.ENTER);
+        verifyThat("Aceptar", NodeMatchers.isVisible());
+        clickOn("Aceptar");
+    }
+
+    @Test
+    public void testI_AlertaTelefonoLargo() {
+        Node rowOrdenarTelefono = lookup("#tcTelefono").nth(0).query();
+        clickOn(rowOrdenarTelefono);
+        Node rowTelefono = lookup("#tcTelefono").nth(1).query();
+        doubleClickOn(rowTelefono);
+        write("615478520888");
+        this.push(KeyCode.ENTER);
+        verifyThat("Aceptar", NodeMatchers.isVisible());
+        clickOn("Aceptar");
+    }
+
+    @Test
+    public void testJ_AlertaTelefonoCorto() {
+        Node rowOrdenarTelefono = lookup("#tcTelefono").nth(0).query();
+        clickOn(rowOrdenarTelefono);
+        Node rowTelefono = lookup("#tcTelefono").nth(1).query();
+        doubleClickOn(rowTelefono);
+        write("6");
+        this.push(KeyCode.ENTER);
+        verifyThat("Aceptar", NodeMatchers.isVisible());
+        clickOn("Aceptar");
+    }
+
+    @Test
+    public void testK_AlertaDescripcion() {
+        Node rowOrdenarDescripcion = lookup("#tcDescripcion").nth(0).query();
+        clickOn(rowOrdenarDescripcion);
+        Node rowDescripcion = lookup("#tcDescripcion").nth(1).query();
+        doubleClickOn(rowDescripcion);
+        write(".");
+        this.push(KeyCode.ENTER);
+        verifyThat("Aceptar", NodeMatchers.isVisible());
+        clickOn("Aceptar");
+    }
+
+    @Test
+    public void testL_ProveedorYaExiste() {
+        clickOn("#btnAltaProveedor");
+        Node rowOrdenarEmpresa = lookup("#tcEmpresa").nth(0).query();
+        clickOn(rowOrdenarEmpresa);
+        Node rowEmpresa = lookup("#tcEmpresa").nth(1).query();
+        doubleClickOn(rowEmpresa);
+        write("Puma");
+        this.push(KeyCode.ENTER);
+        verifyThat("Aceptar", NodeMatchers.isVisible());
+        clickOn("Aceptar");
+    }
+
+    @Test
+    public void testM_botonBorrarError() {
+        clickOn("#btnBorrarProveedor");
+        verifyThat("Aceptar", NodeMatchers.isVisible());
+        clickOn("Aceptar");
+        verifyThat("Aceptar", NodeMatchers.isVisible());
+        clickOn("Aceptar");
+    }
+
+    @Test
+    public void testN_menuListaVendedores() {
+        clickOn("#menuVendedor");
+        clickOn("#menuVendedores");
+        verifyThat("#pnInicioAdminVend", isVisible());
+    }
+
+    @Test
+    public void testO_menuSalir() {
         clickOn("#menuPerfil");
         clickOn("#menuSalir");
         verifyThat("Aceptar", NodeMatchers.isVisible());
         verifyThat("Cancelar", NodeMatchers.isVisible());
         clickOn("Aceptar");
         verifyThat("#pnPrincipal", isVisible());
-    }
-
-    @Test
-    public void testD_menuListaVendedores() {
-        clickOn("#menuVendedor");
-        clickOn("#menuVendedores");
-        verifyThat("#pnInicioAdminVend", isVisible());
-    }
-    
-    @Test
-    public void testE_botonAlta(){
-        clickOn("#btnAltaProveedor");
-        //Node row = lookup("#columnAge").nth(rowIWantToClick).query(); clickOn(node);
-        verifyThat("#btnBorrarProveedor", isEnabled());
-        
-    }
-    
-    @Test
-    public void testD_botonBorrar(){
-        
     }
 
 }
--- src/applicationClient/ApplicationClient.java
+++ src/applicationClient/ApplicationClient.java
@@ -5,7 +5,6 @@
  */
 package applicationClient;
 
-import controllers.InicioAdministradorProveedorController;
 import controllers.LogInController;
 import java.io.IOException;
 import java.util.logging.Level;
@@ -29,11 +28,11 @@ public class ApplicationClient extends Application {
 
         try {
             LOG.log(Level.INFO, "Iniciando la ventana");
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/inicioAdministrador_proveedor.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/LogIn.fxml"));
             LOG.log(Level.INFO, "Cargando Parent");
             Parent root = (Parent) loader.load();
             LOG.log(Level.INFO, "Cargando controller");
-            InicioAdministradorProveedorController controller = ((InicioAdministradorProveedorController) loader.getController());
+            LogInController controller = ((LogInController) loader.getController());
             LOG.log(Level.INFO, "Iniciando controller");
             controller.setStage(primaryStage);
             controller.initStage(root);
